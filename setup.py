deb2b007d42ac702fa2a75ad7c3c5a84582268f2
Sean Hammond <git@seanh.cc>
2015-02-22 22:38:08 +0000

    Change version number to one that works with pip

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.0b1',
+    version='1.0.0',
 
     description='Fast desktop window switching and app launching utility',
     long_description=long_description,
